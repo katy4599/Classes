63b0435c652dc9529f17b390f860e4e46d8c8b56
katy millard <katy.millard@gmail.com>
2022-02-16 10:24:05 -0600

    second commit

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 class Pet {
-    constructor(name, breed) {
+    constructor(name) {
         this.name = name;
-        this.breed = breed;
+        this.species = [];
+        this.age = [];
+        this.breed = [];
+    }
+        addSpecies(newSpecies) {
+            this.species.push(newSpecies);
+            return this;
+        } addAge(newAge) {
+            this.age.push(newAge);
+            return this;
+        } addBreed(newBreed) {
+            this.breed.push(newBreed);
+            return this;
+        } setName(newName) {
+            this.name = newName;
+            return this;
+        }
+    describe() {
+        console.log(`My ${this.species}, ${this.name}, is a ${this.age} year old ${this.breed}.`)
     }
 }
-
 function main() {
-    const myPet = new Pet('Gunner', 'mutt');
-    
-    console.log(myPet);
+    const myPet = new Pet('Gunner');
+        myPet.addSpecies('dog')
+             .addAge('4')
+             .addBreed('mutt');
+
+    myPet.describe();
 }
 
 main();
