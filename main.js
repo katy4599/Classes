06b5b2c90c2a4d142e542a99545cf6f394c830e3
katy millard <katy.millard@gmail.com>
2022-02-16 13:26:17 -0600

    What is the password?

--- main.js
+++ main.js
@@ -1,53 +1,23 @@
-class Pet {
-    constructor(name) {
-        this.name = name;
-        this.species = [];
-        this.age = [];
-        this.breed = [];
-        this.gender = [];
-    }
-    addSpecies(newSpecies) {
-        this.species.push(newSpecies);
-        return this;
-    } 
-    addAge(newAge) {
-        this.age.push(newAge);
-        return this;
-    } 
-    addBreed(newBreed) {
-        this.breed.push(newBreed);
-        return this;
-    }
-    addGender(newGender){
-        this.gender.push(newGender);
-        return this;
-    } 
-    setName(newName) {
-        this.name = newName;
-        return this;
-    }
-    describe() {
-        if (this.gender == 'male') {
-            console.log(
-                `My ${this.species}, ${this.name},`
-                + `is ${this.age}years old, and he is a ${this.gender} ${this.breed}.`
-            );
+
+import input from 'input'
+
+class App {
+    constructor(password) {
+        this.password = password;
+    }   
+
+    async main() {
+        const resp = await input.text('What is the password?')
+        
+        if (resp == 'My Password') {
+
+            console.log('That is correct!')
         } else {
-            console.log(
-                `My ${this.species}, ${this.name},`
-                + `is ${this.age}years old, and she is a ${this.gender} ${this.breed}.`
-            )
+
+            console.log('Try again')
         }
     }
 }
-function main() {
-    const myPet = new Pet('Gunner');
-        myPet.addSpecies('dog')
-             .addAge('4')
-             .addBreed('mutt')
-             .addGender('male');
 
-    myPet.describe();
-}
-
-main();
+const app = new App('My Password');
+app.main();
