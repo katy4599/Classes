a8aab925b360d1a18c6dc01ee878d3f96a2cd16b
katy millard <katy.millard@gmail.com>
2022-02-16 10:26:50 -0600

    Fixed formatting

--- main.js
+++ main.js
@@ -5,19 +5,22 @@ class Pet {
         this.age = [];
         this.breed = [];
     }
-        addSpecies(newSpecies) {
-            this.species.push(newSpecies);
-            return this;
-        } addAge(newAge) {
-            this.age.push(newAge);
-            return this;
-        } addBreed(newBreed) {
-            this.breed.push(newBreed);
-            return this;
-        } setName(newName) {
-            this.name = newName;
-            return this;
-        }
+    addSpecies(newSpecies) {
+        this.species.push(newSpecies);
+        return this;
+    } 
+    addAge(newAge) {
+        this.age.push(newAge);
+        return this;
+    } 
+    addBreed(newBreed) {
+        this.breed.push(newBreed);
+        return this;
+    } 
+    setName(newName) {
+        this.name = newName;
+        return this;
+    }
     describe() {
         console.log(`My ${this.species}, ${this.name}, is a ${this.age} year old ${this.breed}.`)
     }
